9e5ddca64e43d7f9b5b857c6aff59e47a92f4f18
Arron Nestor <33353452+NesTar6@users.noreply.github.com>
2021-07-31 13:48:25 -0400

    added routes and firebase config to express server, set up env variables

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require('../config')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+  const { company } = req.query 
+  firebase.db.collection("companies").add({
+     companyName: company,
+   })
+   .then((docRef) => {
+    res.status(200).send(JSON.stringify({message: `Document written with ID: ${docRef.id}`}));
+   })
+   .catch((error) => {
+    res.status(400).send(JSON.stringify({message: `Error adding document: ${error}`}));
+   });
+})
+
 
 module.exports = router;
