375a5aa8ee03248f403637699ebd3671903a2e45
ayemmoe <amoe.aye@gmail..com>
2021-07-31 13:53:04 -0700

    update package.json

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
+import PhoneSignIn from './PhoneSignIn';
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Text>Welcome Home</Text>
+      <PhoneSignIn></PhoneSignIn>
     </View>
   );
 }
