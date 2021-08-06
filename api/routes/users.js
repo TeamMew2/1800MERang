58f0afafb0beda43e2ae6ce680e4bb39e42b5c0b
Arron Nestor <33353452+NesTar6@users.noreply.github.com>
2021-08-05 21:44:25 -0400

    added favorites route and firebase integration

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require('../config')
+var dotenv = require('dotenv');
+const fetch = require('node-fetch')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  const { companyName, userId, phoneNumber } = req.query   
+
+   firebase.db.collection("favorites").add({
+      companyName: companyName,
+      userId: userId,
+      phoneNumber: phoneNumber
+    })
+    .then((docRef) => {
+     res.status(200).send(JSON.stringify({message: `Document written with ID: ${docRef.id}`}));
+    })
+    .catch((error) => {
+     res.status(400).send(JSON.stringify({message: `Error adding document: ${error}`}));
+    });
 });
 
 module.exports = router;
