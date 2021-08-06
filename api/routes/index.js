91fb5bbfaa4b7b18c9cbd40e86729bb4fc834727
Aye Moe <74073400+ayemmoe@users.noreply.github.com>
2021-08-05 21:55:58 -0400

    removed API keys

--- api/routes/index.js
+++ api/routes/index.js
@@ -14,11 +14,11 @@ router.get('/', function(req, res, next) {
   console.log(company, lat, lng)  
   console.log("URL ", "https://maps.googleapis.com/maps/api/place/textsearch/json?query=",company,"&location=",lng,",",lat,"&",process.env.AYE_APP)
 
-  fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${company}&location=${lng},${lat}&radius=10000&key=AIzaSyDAa97o9R3dHjlHO6Paj2n-WAEMcWIBYs0`)
+  fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${company}&location=${lng},${lat}&radius=10000&key=${process.env.AYE_APP}`)
   .then(result => result.json())
   .catch(err => console.log(err))
   .then(result => {
-      fetch(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${result.results[0].place_id}&key=AIzaSyDAa97o9R3dHjlHO6Paj2n-WAEMcWIBYs0`)
+      fetch(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${result.results[0].place_id}&key=${process.env.AYE_APP}`)
       .then(result => result.json())
       .then(result => {
         return result.result.formatted_phone_number
